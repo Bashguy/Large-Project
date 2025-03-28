27f46b12c3ae78b4d42beb1becfcc4e9e24d5dad
Artful-Donut <lynne.nguyen@gmail.com>
2025-03-28 17:16:29 -0400

    i tried to make collection.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Background from './components/Background'
 import Register from './pages/Register'
 import Home from './pages/Home'
+import { Routes, Route, Link } from "react-router-dom"
+import Collection from './pages/Collection'
 
 function App() {
   const [ auth, setAuth ] = useState(true)
@@ -10,8 +12,12 @@ function App() {
     <div>
       <Background />
 
-      {!auth && <Register />}
-      {auth && <Home />}
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Collection />} />
+      </Routes>
+
     </div>
   )
 }
---
+++ frontend/src/pages/Collection.jsx
@@ -0,0 +1,19 @@
+const Collection = () => {
+  return (
+    <div>
+        {/* Unfied Pantry  Item*/}
+        <div className='ab'> 
+            {/* Top Info Bar */}
+            <div className='fixed w-3/4 bg-white'>
+                <div className=''>
+                    <h2>Your Collection</h2>
+                </div>
+            
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Collection
