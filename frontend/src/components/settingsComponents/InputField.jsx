f8eb4060d6ddfbd14224dc1d16296769d09916cc
Artful-Donut <lynne.nguyen@gmail.com>
2025-04-12 22:11:59 -0400

    added toggleable inputs. idk if it is right tho :<

--- frontend/src/components/settingsComponents/InputField.jsx
+++ frontend/src/components/settingsComponents/InputField.jsx
@@ -4,21 +4,32 @@ import pencil from '../../assets/pencil.png'
 
 
 function InputField({ field, placeholder }) {
-    const [disabled, setDisabled] = useState(false)
+    const [disabled, setDisabled] = useState(true)
+    const [input, setInput] = useState("")
 
     const changeDisabled = () => setDisabled(d => !d)
     
+    const changeField = () => {
+      if(input !== ""){
+        alert(`Changed to ${input}`)
+      }
+    }
 
 
   return (
     <div className="flex w-full justify-between items-center">
         <label className='' for="name" >{field}</label>
         <img className='w-10 h-10'
-        src={'../../assets/pencil.png'} 
-        onClick={changeDisabled}
+        src={'frontend\src\assets\pencil.png'} 
+        onClick={() => {
+          if(!disabled) {
+            changeField()
+          } 
+          changeDisabled()
+        }}
         />
         <input disabled={disabled} 
-        
+        onChange={(e) => setInput(e.target.value)}
         type="text" n="name" 
         id="name" placeholder={placeholder} 
         className="w-3/5 p-2 disabled:bg-[#F1BF7E] bg-white"/>
